1643e0a7596229a77a0d3bf5925724e174afbd65
Anboias <biasinovschi@gmail.com>
2020-08-22 21:26:38 +0300

    removed contact from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="home"/>
       <h1>Hello,</h1>
       <h2>I'm Bogdan, bla bla bla, bla bla bla</h2>
-      <p>Need a developer? <Link to="/contact">Contact me</Link></p>
     </Layout>
   )
 }
