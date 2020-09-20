db6323425c40f260b34f8267e9cf50de25daa956
Anboias <biasinovschi@gmail.com>
2020-09-20 22:06:09 +0300

    made container non-fluid - layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ import "../styles/index.scss"
 
 const Layout = props => {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <Header />
       <div className="row borderDISABLED border-info p-0 m-0">
         <div className="col-12 p-0 m-0">
